ee2249f3a50d38e7d7f04f2b85e5875f473c9a5a
loopyhole <naaperuamitabh@gmail.com>
2022-11-24 20:02:03 +0530

    pipes

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@
 #include <sstream>
 #include <sys/wait.h>
 #include <fstream>
+#include <fcntl.h>
 using namespace std;
 class Config
 {
@@ -42,6 +43,8 @@ void disable_shell()
 }
 void enable_shell()
 {
+    atexit (disable_shell);
+
     struct termios raw = config.orig_termios;
     atexit(disable_shell);
     tcgetattr(STDIN_FILENO, &raw);
@@ -122,16 +125,139 @@ vector<Command> process_commands(string shell_i)
     return cmds;
 }
 
+int start_command (vector <Command> commands){
+    for (int i=0; i<commands.size () - 1; i++){
+        pid_t pid, wpid;
+        int status;
+
+        int pd [2];
+
+        if (pipe (pd) < 0){
+            cerr<<"pipe failed"<<endl;
+        }
+
+        char* args [commands [i].instructions.size ()+1];
+
+        for (int i=0; i<commands [i].instructions.size (); i++){
+            args [i] = new char [commands [i].instructions [i].length ()];
+            strcpy (args [i], commands [i].instructions [i].c_str ()); 
+            cout<<args [i]<<" ";
+        }
+        cout<<endl;
+        args [commands [i].instructions.size ()] = NULL;
+
+        pid = fork ();
+
+        switch (pid){
+            case -1: 
+                cerr<<"fork failed"<<endl;
+            break;
+
+            case 0:
+                dup2 (pd [1], 1);
+                close (pd [0]);
+                close (pd [1]);
+
+                if (execvp (args [0], args) == -1){
+                    cerr<<"command failed"<<endl;
+                }
+                for (int i = 0; i< commands [i].instructions.size ()+1; i++)
+                    delete (args [i]);
+            break;
+
+            default:
+                do {
+                    wpid = waitpid (pid, &status, WUNTRACED);
+                } while (!WIFEXITED (status) && !WIFSIGNALED (status));
+            break;
+        }
+
+        dup2 (pd [0], 0);
+        close (pd [1]);
+        close (pd [0]);
+    }
+    
+    pid_t pid, wpid;
+    int status;
+    int n = commands.size () - 1;
+
+    char* args [commands [n].instructions.size ()+1];
+
+    for (int i=0; i<commands [n].instructions.size (); i++){
+        args [i] = new char [commands [n].instructions [i].length ()];
+        strcpy (args [i], commands [n].instructions [i].c_str ()); 
+        cout<<args [i]<<" ";
+    }
+    cout<<endl;
+    args [commands [n].instructions.size ()] = NULL;
+
+    bool redirectFlag = 0;
+
+    if (args [1] != NULL){
+        int index;
+        for (int i = 0; i<commands [n].instructions.size (); i++){
+            if (strcmp (args [i], ">") == 0 || strcmp (args [i], ">>") == 0){
+                index = i;
+                redirectFlag = 1;
+                break;
+            }
+        }
+
+        if (redirectFlag){
+            int fd;
+            if (strcmp (args [index], ">") == 0){
+                fd = open (args [index + 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+            } else {
+                fd = open (args [index + 1], O_CREAT | O_WRONLY | O_APPEND, 0644);
+            }
+            dup2 (fd, 1);
+            args [index] = 0;
+            if (execvp(args[0], args) == -1)
+                cerr<<"command failed";
+            close(fd);
+        }
+    }
+
+    if (!redirectFlag){
+        if (execvp (args [0], args) == -1){
+            cerr<<"command failed"<<endl;
+        }
+        for (int i = 0; i< commands [n].instructions.size ()+1; i++)
+            delete (args [i]);
+    }
+
+    return 1;
+}
+
 int main(int argc, char const *argv[])
 {
 
     enable_shell();
     while (1)
     {   string command=read_command();
-        if(command=="quit")
+        if(command=="exit"){
             break;
+        }
         vector<Command> cmds=process_commands(command);
 
+        pid_t pid = fork ();
+        pid_t wpid;
+        int status = 0;
+
+        switch (pid){
+            case -1:
+                cerr<<"fork error"<<endl;
+            break;
+
+            case 0:
+                start_command (cmds);
+
+            default:
+                do {
+                    wpid = waitpid (pid, &status, WUNTRACED);
+                } while (!WIFEXITED (status) && !WIFSIGNALED (status));
+            break;
+        }
     }
     return 0;
 }
